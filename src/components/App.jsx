63d42ea0593fa6e994f530be51c26c45cbc01982
Cyberons <platon.pustovoyt@gmail.com>
2024-04-18 20:48:13 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
 import axios from 'axios';
+import { fetchImages } from "./image-api.js";
 import ImageGallery from './ImageGallery/ImageGallery.jsx';
 import ImageModal from './ImageModal/ImageModal.jsx';
 import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn.jsx';
@@ -14,27 +15,27 @@ const App = () => {
   const [selectedImage, setSelectedImage] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    const fetchImages = async () => {
+    if (!query) {
+      return;
+    }
+    const getImages = async () => {
       try {
+        setError(false);
         setLoading(true);
-        const response = await axios.get(`https://api.unsplash.com/photos?page=${page}&per_page=10&client_id=your_access_key`);
-        const data = response.data;
-        setImages(prevImages => [...prevImages, ...data]);
-        setTotalPages(response.headers['x-total-pages']);
-        setLoading(false);
-        setError(null);
+
+        const newImgs = await fetchImages(query, page);
+        setImages((prevImages) => [...prevImages, ...newImgs]);
       } catch (error) {
-        console.error('Error fetching images:', error);
+        setError(true);
+      } finally {
         setLoading(false);
-        setError('Failed to fetch images. Please try again later.');
       }
     };
-
-    fetchImages();
-  }, [page]);
+    getImages();
+  }, [query, page]);
 
   const handleLoadMore = () => {
     setPage(prevPage => prevPage + 1);
@@ -48,15 +49,15 @@ const App = () => {
     setSelectedImage(null);
   };
 
-  const handleSearchSubmit = (query) => {
-    setSearchQuery(query);
-    setPage(1); // Reset page number when performing a new search
+  const handleSubmit = (query) => {
+    setQuery(query);
+    setPage(1);
+    setImages([]);
   };
-
   return (
     <div>
-      <h1>Image Gallery</h1>
-      <SearchBar onSubmit={handleSearchSubmit} />
+      <SearchBar onSubmit={handleSubmit} />
+      {images.length > 0 && <ImageGallery items={images} />}
       {error && <ErrorMessage message={error} />}
       <ImageGallery images={images} onImageClick={handleImageClick} />
       {loading && <p>Loading...</p>}
